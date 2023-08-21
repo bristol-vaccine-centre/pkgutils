d4216b0df2b2f20e893033eebd41d93e9c877b43
robchallen <rob@terminological.co.uk>
2023-08-21 14:25:26 +0100

    add in check_* functions (int/numeric)

---
+++ R/check_numeric.R
@@ -0,0 +1,29 @@
+#' Checks a set of variables can be coerced to integer and coerces them
+#' 
+#' N.B. This only works for the specific environment (to prevent weird side effects)
+#'
+#' @inheritParams check_integer
+#' @return nothing. called for side effects. throws error if not all variables can be coerced.
+#' @export
+#'
+#' @examples
+#' a = c(1:4L)
+#' b = c("1",NA,"3.3")
+#' f = NULL
+#' g = NA
+#' check_numeric(a,b,f,g)
+#' 
+#' c = c("dfsfs")
+#' try(check_numeric(c,d, mean))
+check_numeric = function(..., .message="`{param}` is non-numeric ({err}).", .env = rlang::caller_env()) {
+  predicate = ~ is.numeric(.x)
+  convert = function(tmp) {
+    x = tryCatch(
+      as.numeric(tmp), 
+      error = function(e) stop("error casting to numeric"),
+      warning = function(w) stop("non numeric format")
+    )
+    return(x)
+  }
+  .check_framework(..., predicate = predicate, convert = convert, .message = .message, .env=.env)
+}
---
+++ R/escalateWarnings.R
@@ -0,0 +1,15 @@
+#' Cause warnings to create an error
+#'
+#' @param expr expression to evaluate
+#'
+#' @return the evaluated expression of an error
+#' @export
+#'
+#' @examples
+#' escalateWarnings(as.integer("ASDAS"))
+escalateWarnings = function(expr) {
+  withCallingHandlers(
+    expr, 
+    warning = function(w) stop(w$message,call. = FALSE)
+  )
+}
